c94a4ee8d068f937ae0425c1afc277789d9339f9
Franco Contreras~ <24.f.contreras@gmail.com>
2023-06-18 00:13:33 -0400

    added database placeholder data

---
+++ database/databaseCreation.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE booket;
+
+CREATE TABLE usuarios(
+	id VARCHAR(200) PRIMARY KEY,
+	email VARCHAR(200) NOT NULL UNIQUE,
+	username VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(200) NOT NULL,
+	avatar VARCHAR(200) DEFAULT 'default_avatar.png'
+);
+
+
+CREATE TABLE productos(
+	id VARCHAR(200) PRIMARY KEY,
+	titulo VARCHAR(200) NOT NULL,
+	autor VARCHAR(100) NOT NULL,
+	editorial VARCHAR(100) NOT NULL,
+	paginas INT NOT NULL,
+	estado VARCHAR(50) NOT NULL,
+	encuadernacion VARCHAR (50) NOT NULL,
+	portada VARCHAR(100) DEFAULT 'default_cover.png',
+	idioma VARCHAR(50),
+	stock INT,
+	precio INT,
+	descripcion TEXT DEFAULT 'Sin descripción',
+	vendedor VARCHAR(200),
+	activo BOOLEAN DEFAULT 'true',
+	fecha DATE DEFAULT CURRENT_DATE,
+	CONSTRAINT key_usuario FOREIGN KEY(vendedor) REFERENCES usuarios(id)
+);
+
+CREATE TABLE favoritos(
+	id SERIAL PRIMARY KEY,
+	id_usuario VARCHAR(200) NOT NULL,
+	id_producto VARCHAR(200) NOT NULL,
+	CONSTRAINT key_usuario FOREIGN KEY(id_usuario) REFERENCES usuarios(id),
+	CONSTRAINT key_producto FOREIGN KEY(id_producto) REFERENCES productos(id)
+)
